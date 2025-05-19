ae37e20d0f127c021320db058a154124bd32163f
Mybread2 <ckwnsgh4@naver.com>
2025-05-19 19:21:00 +0900

    start

---
+++ src/main/java/com/example/schedulemanagementappupgrade/ScheduleManagementAppUpgradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagementappupgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementAppUpgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementAppUpgradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulemanagementappupgrade/ScheduleManagementAppUpgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagementappupgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementAppUpgradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
