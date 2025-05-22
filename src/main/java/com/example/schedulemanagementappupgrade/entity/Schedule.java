9911e8a146865ae7ef49c16f9ff990e224d9a6f4
Mybread2 <ckwnsgh4@naver.com>
2025-05-22 17:10:46 +0900

    cascade를 활용하여 유저 삭제시 그 유저가 작성한 일정과 댓글이 다 삭제되도록 구현

--- src/main/java/com/example/schedulemanagementappupgrade/entity/Schedule.java
+++ src/main/java/com/example/schedulemanagementappupgrade/entity/Schedule.java
@@ -3,6 +3,9 @@ package com.example.schedulemanagementappupgrade.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Table(name = "schedule")
@@ -22,9 +25,11 @@ public class Schedule extends BaseEntity {
     private String contents;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    private final List<Comment> comments = new ArrayList<>();
 
     public Schedule() {}
 
